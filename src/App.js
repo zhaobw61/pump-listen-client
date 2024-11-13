3b240ee772d88fa362fe8e1b38d698dae8ddbdb6
bowen <boowenzhao641@gmail.com>
2024-11-13 15:36:11 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react';
+import { Space, Table, Tag } from 'antd';
+const columns = [
+  {
+    title: '名称',
+    dataIndex: 'ticker',
+    key: 'ticker',
+  },
+  {
+    title: '地址',
+    dataIndex: 'address',
+    key: 'address',
+  },
+  {
+    title: '创建时间',
+    dataIndex: 'creationTime',
+    key: 'creationTime',
+  },
+  {
+    title: '推特',
+    dataIndex: 'twitterAccount',
+    key: 'twitterAccount',
+  },
+];
+const data = [
+  {
+    ticker: 'John Brown',
+    address: '5kvHPBLeGiDVkRHzrFJ3NA3FFne17DPSZrrBLzqcpump',
+    creationTime: 1731389388014,
+    twitterAccount: 'https://x.com/ProphecySolana',
+  },
+];
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Table columns={columns} dataSource={data} />
     </div>
   );
 }
