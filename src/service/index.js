6a1d320f6c32ac52eab14a71aa62026093f8e67a
bowen <boowenzhao641@gmail.com>
2024-11-14 19:02:38 +0800

    fix

--- src/service/index.js
+++ src/service/index.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
+const axiosInstance = axios.create({
+  baseURL: '/api',
+});
 // 获取交易对列表
 export const getPairListService = async (pageIndex, pageSize) => {
-  const res = await axios.get('/pairlist', {
+  const res = await axiosInstance.get('/pairlist', {
     params: {
       pageIndex,
       pageSize,
@@ -12,14 +15,14 @@ export const getPairListService = async (pageIndex, pageSize) => {
 
 // 更新单个交易对
 export const updatePairInfoService = async (info) => {
-  const res = await axios.post('/updatePair', {
+  const res = await axiosInstance.post('/updatePair', {
     ...info,
   });
 };
 
 // 删除交易对
 export const deletePairService = async (address) => {
-  const res = await axios.post('/deletePair', {
+  const res = await axiosInstance.post('/deletePair', {
     address: address,
   });
 };
